f82985a3e48a556aea51273da997cb418343abd8
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-10-05 16:05:02 -0500

    Working tie condition

--- script.js
+++ script.js
@@ -20,12 +20,20 @@ const gameBoard = (() => {
   squares.forEach((square) => {
     square.addEventListener('click', (e) => {
       if (board[`${e.target.id}`]) return;
-      //   if (game.winner) return;
+      if (this.winnerDeclared) return;
       board[`${e.target.id}`] = `${game.activePlayer.letter}`;
+      game.remainingSpots--;
       displayBoard();
-      game.switchTurn();
-      game.updateTurnDisplay();
       game.checkWinner();
+      if (game.winnerDeclared === false) {
+        if (game.remainingSpots > 0) {
+          game.switchTurn();
+          game.updateTurnDisplay();
+          console.log(game.remainingSpots);
+        } else {
+          game.declareTie();
+        }
+      }
     });
   });
 
@@ -37,6 +45,8 @@ const game = (() => {
   const playerTwo = createPlayer('Player 2', 'O');
 
   let activePlayer = playerOne;
+  let winnerDeclared = false;
+  let remainingSpots = 9;
 
   function switchTurn() {
     if (this.activePlayer === playerOne) {
@@ -55,7 +65,7 @@ const game = (() => {
     checkRowWinner();
     checkColumnWinner();
     checkDiagonalWinner();
-    checkDraw();
+    checkTie();
   }
 
   function checkRowWinner() {
@@ -66,6 +76,7 @@ const game = (() => {
     ) {
       winner = `${gameBoard.board[0]}`;
       showWinner();
+      this.winnerDeclared = true;
     } else if (
       gameBoard.board[3] === gameBoard.board[4] &&
       gameBoard.board[3] === gameBoard.board[5] &&
@@ -73,6 +84,7 @@ const game = (() => {
     ) {
       winner = `${gameBoard.board[3]}`;
       showWinner();
+      winnerDeclared = true;
     } else if (
       gameBoard.board[6] === gameBoard.board[7] &&
       gameBoard.board[6] === gameBoard.board[8] &&
@@ -80,6 +92,7 @@ const game = (() => {
     ) {
       winner = `${gameBoard.board[6]}`;
       showWinner();
+      winnerDeclared = true;
     }
   }
 
@@ -126,18 +139,29 @@ const game = (() => {
     }
   }
 
-  let filled;
-  function checkDraw() {
-    for (let i = 0; i < gameBoard.board.length; i++) {
-      if (!gameBoard.board[i]) {
-        filled = false;
-        break;
-      } else filled = true;
-    }
-    if (filled && !winner) {
-      turnDisplay.textContent = "It's a tie!";
+  function checkTie() {
+    if (remainingSpots === 0) {
+      declareTie();
+      console.log('Tie');
     }
   }
+
+  function declareTie() {
+    turnDisplay.textContent = "It's a tie!";
+    console.log('declare');
+  }
+
+  //   let filled;
+  //   function checkDraw() {
+  //     for (let i = 0; i < gameBoard.board.length; i++) {
+  //       if (!gameBoard.board[i]) {
+  //         filled = false;
+  //         break;
+  //       } else filled = true;
+  //     }
+  //     if (filled && !winner) {
+  //     }
+  //   }
 
   function showWinner() {
     turnDisplay.textContent = `Player ${winner} has won!`;
@@ -149,7 +173,11 @@ const game = (() => {
     updateTurnDisplay,
     checkWinner,
     winner,
-    checkDraw,
+    // checkDraw,
     showWinner,
+    remainingSpots,
+    winnerDeclared,
+    checkTie,
+    declareTie,
   };
 })();
